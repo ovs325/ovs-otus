352aaf1f3cf48cc514a7663344022586a7655a0f
ovs <50255542@gmail.com>
2024-03-31 22:26:28 +0300

    feat(hw01_hello_otus): Выполнил ДЗ 1

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	str := "Hello, World!"
+	reversed := stringutil.Reverse(str)
+	fmt.Println(reversed)
 }
