af4b711308f9f28604fc7488902f050f14164881
ovs <50255542@gmail.com>
2024-04-16 10:02:51 +0300

    hw03_frequency_analysis

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,39 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(text string) []string {
+	type Word struct {
+		Word      string
+		Frequency int
+	}
+	textMap := make(map[string]int, 0)
+	for _, word := range strings.Fields(text) {
+		if _, ok := textMap[word]; !ok {
+			textMap[word] = 1
+		} else {
+			textMap[word]++
+		}
+	}
+	words := make([]Word, 0)
+	for key, val := range textMap {
+		words = append(words, Word{key, val})
+	}
+	sort.Slice(words, func(i, j int) bool {
+		if words[i].Frequency == words[j].Frequency {
+			return words[i].Word < words[j].Word
+		}
+		return words[i].Frequency > words[j].Frequency
+	})
+	res := make([]string, 0)
+	for _, word := range words {
+		res = append(res, word.Word)
+	}
+	if len(res) > 10 {
+		return res[:10]
+	}
+	return res
 }
