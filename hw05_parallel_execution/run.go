4f49da2f4b36b20822e45d86eb195e64f4d2872f
ovs <50255542@gmail.com>
2024-05-25 18:33:22 +0300

    hw05_parallel_execution

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -1,6 +1,7 @@
 package hw05parallelexecution
 
 import (
+	"context"
 	"errors"
 )
 
@@ -8,8 +9,73 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
+var taskLauncher = func(ctx context.Context, tsk Task, tCh chan error) {
+	select {
+	case <-ctx.Done():
+		tCh <- ErrErrorsLimitExceeded
+		return
+	case tCh <- tsk():
+		return
+	}
+}
+
+// Run запускает задачи в n подпрограммах и останавливает свою работу при получении m ошибок от задач.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	if m < 0 || n <= 0 {
+		return ErrErrorsLimitExceeded
+	}
+	lenTasks := len(tasks)
+	if lenTasks == 0 {
+		return nil
+	}
+	tChan := make(chan error)
+	exceedErr := 0
+	commonErr := 0
+	success := 0
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if lenTasks > n {
+		for _, task := range tasks[:n] {
+			go taskLauncher(ctx, task, tChan)
+		}
+		tasks = tasks[n:]
+	} else {
+		for _, task := range tasks {
+			go taskLauncher(ctx, task, tChan)
+		}
+		tasks = []Task{}
+	}
+
+	for commonErr+success+exceedErr+len(tasks) < lenTasks {
+		select {
+		case res := <-tChan:
+			if res != nil {
+				if errors.Is(res, ErrErrorsLimitExceeded) {
+					exceedErr++
+					continue
+				}
+				commonErr++
+				if commonErr >= m {
+					cancel()
+					continue
+				}
+				if len(tasks) > 0 {
+					go taskLauncher(ctx, tasks[0], tChan)
+					tasks = tasks[1:]
+				}
+				continue
+			}
+			success++
+			if len(tasks) > 0 {
+				go taskLauncher(ctx, tasks[0], tChan)
+				tasks = tasks[1:]
+			}
+		default:
+		}
+	}
+	if commonErr >= m {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
 }
