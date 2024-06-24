7a295fb654ca3cffa72f4ea0345010602ddcddf1
ovs <50255542@gmail.com>
2024-06-24 22:50:22 +0300

    hw08_envdir_tool

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,16 +1,52 @@
 package main
 
+import (
+	"bytes"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
-// EnvValue helps to distinguish between empty files and files with the first empty line.
+// EnvValue помогает отличить пустые файлы от файлов с первой пустой строкой.
 type EnvValue struct {
 	Value      string
 	NeedRemove bool
 }
 
-// ReadDir reads a specified directory and returns map of env variables.
-// Variables represented as files where filename is name of variable, file first line is a value.
+// ReadDir считывает указанный каталог и возвращает отображение переменных env.
+// Переменные представлены в виде файлов, где filename - это имя переменной, первая строка файла - это значение.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	env := make(Environment)
+	for _, file := range files {
+		if !file.IsDir() {
+			name := file.Name()
+			firstStr, needRemove, err := GetEnvVar(filepath.Join(dir, name))
+			if err != nil {
+				return nil, err
+			}
+			env[name] = EnvValue{Value: firstStr, NeedRemove: needRemove}
+		}
+	}
+	return env, nil
+}
+
+// Считываем содержимое файла, определяем необходимость удаления.
+func GetEnvVar(path string) (string, bool, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return "", false, err
+	}
+	text := string(bytes.ReplaceAll(data, []byte{0x00}, []byte{'\n'}))
+	text = strings.TrimRight(text, " \t")
+	if text == "" {
+		return "", true, nil
+	}
+	return strings.SplitN(text, "\n", 2)[0], false, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,37 @@
 package main
 
-// RunCmd runs a command + arguments (cmd) with environment variables from env.
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+// RunCmd запускает команду + аргументы (cmd) с переменными окружения из env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	envList := make([]string, 0, len(env))
+	for name, value := range env {
+		if value.NeedRemove {
+			envList = append(envList, fmt.Sprintf("unset %s", name))
+		} else {
+			envList = append(envList, fmt.Sprintf("%s=%s", name, value.Value))
+		}
+	}
+	command := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+	command.Env = os.Environ()
+	command.Env = append(command.Env, envList...)
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	err := command.Run()
+	if err != nil {
+		var e *exec.ExitError
+		if errors.As(err, &e) {
+			return e.ExitCode()
+		}
+		fmt.Fprintf(os.Stderr, "Error running command: %v\n", err)
+		return 1
+	}
+	return 0
 }
